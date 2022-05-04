f9a91f30d3ee38000dece41ce6cd1d27836d8d98
Jane <jane.anqi.cui@gmail.com>
2022-05-04 13:35:21 -0400

    add favorites lists

--- app/main/routes.py
+++ app/main/routes.py
@@ -13,6 +13,24 @@ def index():
   restos = Resto.query.all()
   return render_template('home.html', restos = restos)
 
+@main.route('/favorite/<resto_id>', methods=['POST'])
+@login_required
+def favorite(resto_id):
+  user = flask_login.current_user
+  resto = Resto.query.get(resto_id)
+  user.favorites.append(resto)
+  db.session.add(user)
+  db.session.commit()
+  flash('resto added')
+  
+  return redirect(url_for('main.favorites'))
+
+@main.route('/favorites')
+@login_required
+def favorites():
+  user = flask_login.current_user
+  return render_template('favorites_detail.html', user=user)
+
 @main.route('/new_resto', methods=['GET', 'POST'])
 @login_required
 def new_resto():
@@ -30,7 +48,7 @@ def new_resto():
     flash('Resto added!')
     return redirect(url_for('main.index'))
 
-  return render_template('new_resto.html', form=form)
+  return redirect(url_for('main.resto_detail', resto_id=resto_id))
  
 @main.route('/resto/<resto_id>', methods=['GET', 'POST'])
 def resto_detail(resto_id):
