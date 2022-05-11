d01b7e1cfa019dfbd9a3369079b37e9216e5d5d9
Jane <jane.anqi.cui@gmail.com>
2022-05-10 22:09:58 -0400

    dotenv

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 """Initialize Config class to access environment variables."""
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 import os
 
-# load_dotenv()
+load_dotenv()
 
 class Config(object):
     """Set environment variables."""
